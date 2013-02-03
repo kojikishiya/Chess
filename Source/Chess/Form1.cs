c8b1dfe40de70800ff8077e5ee725f71fe79859f
kojikishiya <kishiya_koji@yahoo.co.jp>
2013-02-03 22:39:08 +0900

    desplay player name

--- Source/Chess/Form1.cs
+++ Source/Chess/Form1.cs
@@ -19,13 +19,43 @@ namespace Chess
             InitializeComponent();
 
             var board = new ChessBoard(ImageSize.Thirty);
-
+            board.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
             Text = "Chess";
-            this.Controls.Add(board);
-            ClientSize = board.ClientSize;
-            //ClientSize = new Size(PictureSize * 8, PictureSize * 8);
-            // king = Properties.Resources._30px_Chess_kdt45_svg;
+            //this.Controls.Add(board);
+            ClientSize = new Size(board.ClientSize.Width + 50, board.ClientSize.Height) ;
+            var panel = new FlowLayoutPanel();
+            panel.Size = ClientSize;
+            this.Controls.Add(panel);
 
+            panel.Controls.Add(board);
+            var pnl2 = createPanel(board.ClientSize.Height, 0);
+            panel.Controls.Add(pnl2);
+            pnl2.Controls.Add(createPanel(board.ClientSize.Height/2, 1));
+            pnl2.Controls.Add(createPanel(board.ClientSize.Height / 2, 2));
+
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+
+        }
+        private Control createPanel(int height,int player)
+        {
+            var panel2 = new FlowLayoutPanel();
+            panel2.Size = new Size(50, height);
+            panel2.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            panel2.BackColor = System.Drawing.Color.Moccasin;
+            if (player != 0)
+            {
+                var lablel1 = new Label();
+                lablel1.Text = "Player" + player;
+                panel2.Controls.Add(lablel1);
+            }
+            return panel2;
+
+        }
+        private Button createButton()
+        {
+            var btn = new Button();
+            btn.Size = new Size(45, 20);
+            return btn;
         }
     }
 }
